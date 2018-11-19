a6ba8b665d6bf2c13872e4cdea76e73bea16cedd
Thies C. Arntzen <thieso@gmail.com>
2018-11-18 18:17:36 -0800

    testing

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
 // swift-tools-version:4.0
- 
+
 import PackageDescription
- 
-let package = Package(
-    name: "AXSwift",
-    targets: [
-        .target(name: "AXSwift", path: "Sources")
-    ]
-)
+
+let pkg = Package(name: "AXSwift")
+pkg.products = [
+    .library(name: "AXSwift", targets: ["AXSwift"]),
+]
+
+let pmk: Target = .target(name: "AXSwift")
+pmk.path = "Sources"
+pmk.exclude = [
+    "Info.plist"
+]
+pkg.swiftLanguageVersions = [4]
+pkg.targets = [
+    pmk
+]
